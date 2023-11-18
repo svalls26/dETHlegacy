50318815f4daac5feed152f4d1bad4166e3280e9
Allard <AllardQuek@users.noreply.github.com>
2023-11-18 16:23:21 +0300

    Update homepage with app details

--- frontend/packages/nextjs/pages/index.tsx
+++ frontend/packages/nextjs/pages/index.tsx
@@ -11,47 +11,28 @@ const Home: NextPage = () => {
         <div className="px-5">
           <h1 className="text-center mb-8">
             <span className="block text-2xl mb-2">Welcome to</span>
-            <span className="block text-4xl font-bold">Scaffold-ETH 2</span>
+            <span className="block text-4xl font-bold">dETH Legacy</span>
           </h1>
-          <p className="text-center text-lg">
-            Get started by editing{" "}
-            <code className="italic bg-base-300 text-base font-bold max-w-full break-words break-all inline-block">
-              packages/nextjs/pages/index.tsx
-            </code>
-          </p>
-          <p className="text-center text-lg">
-            Edit your smart contract{" "}
-            <code className="italic bg-base-300 text-base font-bold max-w-full break-words break-all inline-block">
-              YourContract.sol
-            </code>{" "}
-            in{" "}
-            <code className="italic bg-base-300 text-base font-bold max-w-full break-words break-all inline-block">
-              packages/hardhat/contracts
-            </code>
-          </p>
+          <p className="text-center text-lg">Life has to end. Your assets don&apos;t.</p>
+          <p className="text-center text-lg">Distribute your assets to loved ones, or give back to society</p>
         </div>
 
-        <div className="flex-grow bg-base-300 w-full mt-16 px-8 py-12">
-          <div className="flex justify-center items-center gap-12 flex-col sm:flex-row">
-            <div className="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs rounded-3xl">
+        <div className="bg-base-300 w-full mt-16 px-8 py-12">
+          <div className="flex w-full">
+            <div className="grid h-20 flex-grow card bg-base-300 rounded-box place-items-center">
+              {/* <div className="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs rounded-3xl"> */}
               <BugAntIcon className="h-8 w-8 fill-secondary" />
-              <p>
-                Tinker with your smart contract using the{" "}
-                <Link href="/debug" passHref className="link">
-                  Debug Contract
-                </Link>{" "}
-                tab.
-              </p>
+              <button className="btn btn-accent btn-sm mt-3">Manage Assets</button>
+              {/* </div> */}
             </div>
-            <div className="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs rounded-3xl">
+            <div className="divider lg:divider-horizontal">OR</div>
+            <div className="grid h-20 flex-grow card bg-base-300 rounded-box place-items-center">
+              {/* <div className="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs rounded-3xl"> */}
               <MagnifyingGlassIcon className="h-8 w-8 fill-secondary" />
-              <p>
-                Explore your local transactions with the{" "}
-                <Link href="/blockexplorer" passHref className="link">
-                  Block Explorer
-                </Link>{" "}
-                tab.
-              </p>
+              <Link href="/upload">
+                <button className="btn btn-accent btn-sm mt-3">Upload Death Certificate</button>
+              </Link>
+              {/* </div> */}
             </div>
           </div>
         </div>
